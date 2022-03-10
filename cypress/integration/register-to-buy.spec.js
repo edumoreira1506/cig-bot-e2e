9fb9d016895ef112873c4f454e908c6b3f4ec0f5
Eduardo Moreira <contato@eduardoem.com.br>
2022-03-09 21:38:00 -0300

    Add test

--- cypress/integration/register-to-buy.spec.js
+++ cypress/integration/register-to-buy.spec.js
@@ -1,26 +1,96 @@
-import { breederFactory, userFactory } from '@cig-platform/factories';
+import { breederFactory, poultryFactory, userFactory } from '@cig-platform/factories';
 
 /* eslint-disable cypress/no-unnecessary-waiting */
 it('From register to buy a poultry', () => {
   const user = userFactory({ password: Cypress.env('PASSWORD')  });
   const breeder = breederFactory();
+  const poultry = poultryFactory({ description: 'Cool description here :D' });
 
+  cy.viewport('iphone-5');
   cy.visit(`${Cypress.env('MARKETPLACE_URL')}/login`);
   cy.contains('Registre-se').click();
   cy.wait(1000);
 
-  cy.get('input[name="user-email"]').type(user.email);
-  cy.get('input[name="user-password"]').type(user.password);
-  cy.get('input[name="user-confirm-password"]').type(user.password);
-  cy.get('input[name="user-name"]').type(user.name);
-  cy.contains('Próximo').click();
+  cy.register({ user, breeder });
 
-  cy.get('input[name="breeder-name"]').type(breeder.name);
-  cy.get('input[name="breeder-code"]').type(breeder.code);
-  cy.contains('Cadastrar').click();
+  cy.login({ email: user.email, password: user.password });
+
+  cy.contains('Nova ave').click();
+  cy.get('[name="poultry-type"]').select(poultry.type);
+  cy.get('[name="poultry-gender"]').select(poultry.gender);
+  cy.get('[name="poultry-gender-category"]').select(poultry.genderCategory);
+  cy.get('[name="poultry-crest"]').select(poultry.crest);
+  cy.get('[name="poultry-dewlap"]').select(poultry.dewlap);
+  cy.get('[name="poultry-tail"]').select(poultry.tail);
+  cy.get('[name="poultry-name"]').type(poultry.name);
+  cy.get('[name="poultry-description"]').type(poultry.description);
+  cy.get('[name="poultry-register"]').type(poultry.register);
+  cy.get('[name="poultry-birth-date"]').type(poultry.birthDate.toISOString().split('T')[0]);
+  cy.contains('Salvar').click();
+  cy.aceeptSwal();
   cy.wait(1000);
 
-  cy.get('input[name="email"]').type(user.email);
-  cy.get('input[name="password"]').type(user.password);
-  cy.contains('Entrar').click();
+  cy.get('[data-testid="round"]').last().click();
+  cy.wait(1000);
+  cy.get('ul li a svg').last().click();
+  cy.contains('Anúnciar ave').click();
+  cy.fillSwalInput('150');
+  cy.aceeptSwal();
+
+  cy.wait(1000);
+  cy.aceeptSwal();
+
+  cy.logout();
+
+  const newUser = userFactory();
+  const newBreeder = breederFactory();
+
+  cy.contains('Registre-se').click();
+
+  cy.register({ user: newUser, breeder: newBreeder });
+
+  cy.login({ email: newUser.email, password: newUser.password });
+
+  cy.visit(Cypress.env('MARKETPLACE_URL'));
+
+  cy.get('button:nth-child(2)').first().click();
+  cy.get('input').type(poultry.name).type('{enter}');
+  cy.wait(1000);
+
+  cy.contains(poultry.name).click();
+  cy.scrollTo('bottom');
+  cy.contains('Fazer proposta').click();
+  cy.fillSwalInput('3000');
+  cy.aceeptSwal();
+  cy.fillSwalInput('Bacanudo');
+  cy.aceeptSwal();
+
+  cy.wait(1000);
+  cy.logout();
+
+  cy.login({ email: user.email, password: user.password });
+
+  cy.openMenu();
+  cy.contains('Vendas').click();
+  cy.contains(poultry.name).click();
+  cy.scrollTo('bottom');
+  cy.contains('Confirmar').click();
+  cy.aceeptSwal();
+  cy.wait(1000);
+  cy.aceeptSwal();
+  cy.logout();
+
+  cy.login({ email: newUser.email, password: newUser.password });
+  cy.openMenu();
+  cy.contains('Compras').click();
+  cy.contains(poultry.name).click();
+  cy.scrollTo('bottom');
+  cy.contains('Finalizar').click();
+  cy.aceeptSwal();
+  cy.wait(1000);
+  cy.aceeptSwal();
+
+  cy.openMenu();
+  cy.contains('Meu plantel').click();
+  cy.contains(poultry.name).click();
 });
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,3 +1,5 @@
+/* eslint-disable cypress/no-unnecessary-waiting */
+
 // ***********************************************
 // This example commands.js shows you how to
 // create various custom commands and overwrite
@@ -23,3 +25,41 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+
+Cypress.Commands.add('aceeptSwal', () => {
+  cy.get('.swal2-confirm').click();
+});
+
+Cypress.Commands.add('register', ({ user, breeder }) => {
+  cy.get('input[name="user-name"]').type(user.name);
+  cy.get('input[name="user-email"]').type(user.email);
+  cy.get('input[name="user-password"]').type(user.password);
+  cy.get('input[name="user-confirm-password"]').type(user.password);
+  cy.contains('Próximo').click();
+
+  cy.get('input[name="breeder-name"]').type(breeder.name);
+  cy.get('input[name="breeder-code"]').type(breeder.code);
+  cy.contains('Cadastrar').click();
+  cy.wait(1000);
+  cy.aceeptSwal();
+});
+
+Cypress.Commands.add('login', ({ email, password }) => {
+  cy.get('input[name="email"]').type(email);
+  cy.get('input[name="password"]').type(password);
+  cy.contains('Entrar').click();
+  cy.wait(1000);
+});
+
+Cypress.Commands.add('fillSwalInput', (value) => {
+  cy.get('.swal2-input').type(value);
+});
+
+Cypress.Commands.add('logout', () => {
+  cy.get('[data-testid="round-image-container"]').click();
+  cy.contains('Sair').click();
+});
+
+Cypress.Commands.add('openMenu', () => {
+  cy.get('[data-testid="sandwich-button"]').click();
+});
